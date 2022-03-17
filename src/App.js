7416ae2141f85969b070fc8855a9c0e8957b6a0f
AkashAbrrar <mabrrar9@gmail.com>
2022-03-17 10:27:03 +0600

    folder tree managment system testing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Countries from './components/Countries/Countries';
+import Person from './components/Person/Person';
+import Header from './components/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <LooadCountries></LooadCountries>
+      <Countries></Countries>
+      <Person></Person>
+      <Header></Header>
+
+      {/* <LooadCountries></LooadCountries> */}
     </div>
   );
 }
 
-function LooadCountries() {
-  const [countries, setCountries] = useState([]);
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then(res => res.json())
-      .then(data => setCountries(data))
-  }, [])
+// function LooadCountries() {
+//   const [countries, setCountries] = useState([]);
+//   useEffect(() => {
+//     fetch('https://restcountries.com/v3.1/all')
+//       .then(res => res.json())
+//       .then(data => setCountries(data))
+//   }, [])
 
-  return (
-    <div>
-      <h1>Visiting Every Country In The World :)</h1>
-      <h3>Available Cuontries:{countries.length}</h3>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <h1>Visiting Every Country In The World :)</h1>
+//       <h3>Available Cuontries:{countries.length}</h3>
+//       {/* set Data dynamicaly */}
+//       {
+//         countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+//       }
+//     </div>
+//   )
+// };
+// // make componet for all data display
+// function Country(props) {
+//   return (
+//     <div className="cont-sytle">
+//       <h2>Name:{props.name}</h2>
+//       <h4>Population:{props.population}</h4>
+//     </div>
+//   )
+// }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Welcome My React Website</h1>
+        </div>
+    );
+};
+
+export default Header;
